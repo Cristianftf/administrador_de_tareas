c1427b852a2057ae338e48e350703d17b3cc58cb
Cristianftf <tcristianftf@gmail.com>
2025-10-02 21:50:33 -0400

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/taskadmin/administrador_de_tareas/AdministradorDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.taskadmin.administrador_de_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorDeTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskadmin/administrador_de_tareas/AdministradorDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskadmin.administrador_de_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
